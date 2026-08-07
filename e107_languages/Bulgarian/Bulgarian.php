4708b6500cac654a400aaff1dd7fcc0c8490884e
Veselin Totev <v.totev@probg.com>
2026-08-07 15:24:47 +0300

    feat(i18n): migrate Bulgarian core language file

---
+++ e107_languages/Bulgarian/Bulgarian.php
@@ -0,0 +1,147 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Bulgarian language file - generic terms and system LAN
+ * Professional Bulgarian translation for e107 v2.3.10
+ *
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+*/
+setlocale(LC_ALL, 'bg_BG.UTF-8', 'bg_BG.utf8', 'bul_bgr.utf8', 'bg');
+define("CORE_LC", "bg");
+define("CORE_LC2", "BG");
+// define("TEXTDIRECTION","rtl");
+
+define("CORE_LAN1", "Грешка: избраната тема липсва.\n\nИзберете друга тема от настройките в администрацията или качете файловете на текущата тема на сървъра.");
+define("CORE_LAN4", "Моля, изтрийте install.php от сървъра.");
+define("CORE_LAN5", "Ако не го направите, съществува потенциален риск за сигурността на сайта.");
+define("CORE_LAN6", "Защитата от прекомерни заявки е активирана. Ако продължите да изпращате твърде много заявки, достъпът ви до сайта може да бъде блокиран.");
+define("CORE_LAN7", "Ядрото се опитва да възстанови настройките от автоматично резервно копие.");
+define("CORE_LAN8", "Грешка в основните настройки");
+define("CORE_LAN9", "Ядрото не успя да възстанови настройките от автоматичното резервно копие. Изпълнението е прекратено.");
+define("CORE_LAN10", "Открита е повредена бисквитка. Сесията е прекратена.");
+
+define("CORE_LAN11", "Време за генериране: ");
+define("CORE_LAN12", " сек. (");
+define("CORE_LAN13", "% от времето е за заявки) ");
+define("CORE_LAN14", "%2.3f сек. CPU (%2.2f%% натоварване, %2.3f стартиране). Време: ");
+define("CORE_LAN15", "Заявки към БД: ");
+define("CORE_LAN16", "Памет: ");
+
+define("CORE_LAN17", "[ изображението е деактивирано ]");
+define("CORE_LAN18", "Изображение: ");
+
+define("CORE_LAN_B", "B");
+define("CORE_LAN_KB", "kB");
+define("CORE_LAN_MB", "MB");
+define("CORE_LAN_GB", "GB");
+define("CORE_LAN_TB", "TB");
+
+define("EMESSLAN_TITLE_INFO", "Системна информация");
+define("EMESSLAN_TITLE_ERROR", "Грешка");
+define("EMESSLAN_TITLE_SUCCESS", "Успешно");
+define("EMESSLAN_TITLE_WARNING", "Предупреждение");
+define("EMESSLAN_TITLE_DEBUG", "Системно отстраняване на грешки");
+
+define("LAN_NO_PERMISSIONS", "Нямате необходимите права за достъп до тази страница.");
+define("LAN_EDIT","Редактиране");
+define("LAN_DELETE","Изтриване");
+define("LAN_DEFAULT","По подразбиране");
+define("LAN_MORE", "Още...");
+define("LAN_LESS", "...по-малко");
+define("LAN_READ_MORE", "Прочетете още...");
+define("LAN_GOPAGE", "Към страница");
+define("LAN_GOTOPAGEX", "Към страница [x]");
+define("LAN_GO", "Продължи");
+define("LAN_SUBMIT", "Изпращане");
+define("LAN_NONE", "Няма");
+define("LAN_WARNING", "Предупреждение!");
+define("LAN_ERROR", "Грешка");
+define("LAN_ANONYMOUS", "Анонимен");
+define("LAN_EMAIL_SUBS", "-имейл-");
+define("LAN_ACTIVE","Активно");
+define("LAN_YES", "Да");
+define("LAN_NO", "Не");
+define("LAN_OK", "OK");
+define("LAN_ACTIONS", "Действия");
+define("LAN_THANK_YOU", "Благодарим ви");
+define("LAN_CONTINUE", "Продължи");
+define("LAN_ENTER", "Въведете");
+define("LAN_ENTER_CODE", "Въведете кода");
+define("LAN_INVALID_CODE", "Въведеният код е неправилен.");
+define("LAN_SEARCH", "Търсене");
+define("LAN_VIEW", "Преглед");
+define("LAN_CLICK_TO_VIEW", "Щракнете за преглед");
+define("LAN_SORT", "Сортиране");
+define("LAN_ORDER_BY", "Подреждане по");
+define("LAN_ASCENDING", "Възходящо");
+define("LAN_DESCENDING", "Низходящо");
+define("LAN_SHARE", "Споделяне");
+define("LAN_BACK", "Назад");
+define("LAN_NAME", "Име");
+define("LAN_DESCRIPTION", "Описание");
+define("LAN_CANCEL","Отказ");
+define("LAN_DATE","Дата");
+define("LAN_DATE_POSTED", "Дата на публикуване");
+define("LAN_POSTED_BY", "Публикувано от");
+define("LAN_JSCONFIRM","Сигурни ли сте?");
+define("LAN_IP","IP");
+define("LAN_IP_ADDRESS","IP адрес");
+define("LAN_AUTHOR","Автор");
+define("LAN_CATEGORY", "Категория");
+define("LAN_CATEGORIES", "Категории");
+define("LAN_GUEST", "Гост");
+define("LAN_NEXT", "Следващ");
+define("LAN_PREVIOUS", "Предишен");
+define("LAN_LOGIN", "Вход");
+define("LAN_LOGOUT", "Изход");
+define("LAN_VERIFY", "Потвърждаване");
+define("LAN_SETTINGS", "Настройки");
+define("LAN_PASSWORD", "Парола");
+define("LAN_INCORRECT_PASSWORD", "Неправилна парола");
+define("LAN_TYPE", "Тип");
+define("LAN_SCREENSHOT", "Екранна снимка");
+define("LAN_FILE", "Файл");
+define("LAN_YOUTUBE_VIDEO", "YouTube видео");
+define("LAN_YOUTUBE_PLAYLIST", "YouTube плейлист");
+define("LAN_FILETYPES", "Типове файлове");
+define("LAN_FILE_NOT_FOUND", "Файлът не е намерен");
+define("LAN_FILES","Файлове");
+define("LAN_SIZE", "Размер");
+define("LAN_VERSION", "Версия");
+define("LAN_DOWNLOAD", "Изтегляне");
+define("LAN_DOWNLOAD_NO_PERMISSION", "Файлът не е намерен или нямате право да го изтеглите.");
+define("LAN_WEBSITE", "Уебсайт");
+define("LAN_COMMENTS", "Коментари");
+define("LAN_LOCATION", "Местоположение");
+define("LAN_NO_RECORDS_FOUND","Няма намерени записи");
+define("LAN_RATING", "Оценка");
+define("LAN_IMAGE","Изображение");
+define("LAN_ABOUT", "Относно");
+define("LAN_TITLE", "Заглавие");
+define("LAN_MESSAGE", "Съобщение");
+define("LAN_USER", "Потребител");
+define("LAN_EMAIL","Имейл адрес");
+define("LAN_WROTE", "написа");
+define("LAN_RE_ORDER", "Преподреждане");
+define("LAN_RELATED", "Свързани");
+define("LAN_CLOSE", "Затваряне");
+define("LAN_EXPAND", "Разгъване");
+define("LAN_LIST", "Списък");
+define("LAN_DATESTAMP","Дата");
+define("LAN_SUBJECT","Тема");
+
+define("LAN_ENTER_USRNAME_EMAIL", "Въведете потребителско име или имейл адрес");
+define("LAN_PWD_REQUIRED", "Паролата е задължителна");
+define("LAN_SHOW", "Показване");
+define("LAN_GENERATE", "Генериране");
+define("LAN_SUMMARY", "Обобщение");
+define("LAN_REQUIRED_BLANK", "Едно или повече задължителни полета не са попълнени.");
+define("LAN_PLEASEWAIT", "Моля, изчакайте");
+define("LAN_CHOOSE_FILE", "Изберете файл");
+
+define("LAN_REQUIRED", "Задължително");
+
+define("LAN_DEVELOPERMODE_CHECK", "[b]Режимът за разработка е активиран. Използвайте го само по време на разработка.[/b] [br]Деактивирайте режима за разработка, преди сайтът да бъде използван в продукционна среда. Когато този режим е активен, чувствителна информация може да стане публично достъпна.");
