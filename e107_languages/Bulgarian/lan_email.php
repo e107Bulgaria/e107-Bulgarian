bfaf73b5c8d2666eda3326e779bc9d75cf9bfeed
Veselin Totev <v.totev@probg.com>
2026-08-07 15:25:04 +0300

    feat(i18n): migrate Bulgarian email language file

---
+++ e107_languages/Bulgarian/lan_email.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * e107 website system - Bulgarian language file
+ */
+if (!defined("PAGE_NAME")) { define("PAGE_NAME", "Имейл"); }
+
+define("LAN_EMAIL_1", "От:");
+define("LAN_EMAIL_2", "IP адрес на подателя:");
+define("LAN_EMAIL_3", "Изпратен по имейл материал от ");
+define("LAN_EMAIL_4", "Изпращане на имейл");
+define("LAN_EMAIL_5", "Изпращане на материала до приятел");
+define("LAN_EMAIL_6", "Предполагам, че този материал от следния сайт може да ви бъде интересен:");
+define("LAN_EMAIL_7", "изпращане по имейл");
+define("LAN_EMAIL_8", "Коментар");
+define("LAN_EMAIL_9", "Имейлът не можа да бъде изпратен");
+define("LAN_EMAIL_10", "Имейлът е изпратен до");
+define("LAN_EMAIL_11", "Имейлът е изпратен");
+define("LAN_EMAIL_13", "Изпращане на статия до приятел");
+define("LAN_EMAIL_14", "Изпращане на новина до приятел");
+define("LAN_EMAIL_15", "Потребителско име: ");
+define("LAN_EMAIL_106", "Въведеният имейл адрес не изглежда валиден");
+define("LAN_EMAIL_185", "Изпращане на статия");
+define("LAN_EMAIL_186", "Изпращане на новина");
+define("LAN_EMAIL_187", "Имейл адрес на получателя");
+define("LAN_EMAIL_188", "Предполагам, че тази новина от следния сайт може да ви бъде интересна:");
+define("LAN_EMAIL_189", "Предполагам, че тази статия от следния сайт може да ви бъде интересна:");
+define("LAN_EMAIL_190", "Въведете кода от изображението");
+define("LAN_SOCIAL_LINK_CHK", "Вижте този линк: ");
