8d4d366163daaae3d408f92db8fdabf6747e5b31
Veselin Totev <v.totev@probg.com>
2026-08-07 15:27:40 +0300

    feat(i18n): migrate Bulgarian meta administration

---
+++ e107_languages/Bulgarian/admin/lan_meta.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * e107 website system - Bulgarian administrative language file
+ */
+define("METLAN_00", "Мета и персонализирани тагове");
+
+define("METLAN_1", "Допълнителни meta тагове");
+define("METLAN_2", "например < meta name='revisit-after' content='30 days' />");
+define("METLAN_3", "Използване на заглавието и резюмето на новината като meta description в страниците на новините.");
+
+define("METLAN_4", "Персонализирани тагове (вътре в [x])");
+define("METLAN_5", "Персонализирани тагове (след [x])");
+define("METLAN_6", "Персонализирани тагове (преди [x])");
+define("METLAN_7", "Всички мета данни или персонализирани HTML тагове, въведени тук (например <script> тагове или код за Google Analytics), ще бъдат включени във всяка страница на сайта в съответната област.");
+
+define("METLAN_8", "Максимална дължина на SEO заглавието");
+define("METLAN_9", "Максимална дължина на SEO описанието");
