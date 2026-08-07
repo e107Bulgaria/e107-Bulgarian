e41a01326c7388c588be0ffe8030c6b99e873fbf
Veselin Totev <v.totev@probg.com>
2026-08-07 15:28:10 +0300

    feat(i18n): migrate Bulgarian administrator language file

---
+++ e107_languages/Bulgarian/admin/lan_administrator.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * e107 website system
+ * Bulgarian language file - Administrators
+ * Professional Bulgarian translation for e107 v2.3.10
+ */
+
+define("ADMSLAN_6", "е главният администратор на сайта и не може да бъде изтрит.");
+define("ADMSLAN_13", "Съществуващи администратори");
+define("ADMSLAN_16", "Име на администратора");
+define("ADMSLAN_18", "Права");
+define("ADMSLAN_21", "Промяна на правата на администратор");
+define("ADMSLAN_25", "Качване и управление на файлове");
+define("ADMSLAN_27", "Управление на категориите за връзки");
+define("ADMSLAN_41", "Създаване и редактиране на персонализирани менюта");
+define("ADMSLAN_42", "Публикуване на ревюта");
+define("ADMSLAN_52", "Обновяване на администратора");
+define("ADMSLAN_56", "Администратор на сайта");
+define("ADMSLAN_58", "Главен администратор");
+define("ADMSLAN_59", "Премахване на администраторските права");
+define("ADMSLAN_61", "Администраторът е изтрит");
+define("ADMSLAN_62", "Мениджър на плъгини");
+define("ADMSLAN_71", "Показване на правата");
+define("ADMSLAN_72", "Администратор ID: [x], име: [y], нови права: ");
+define("ADMSLAN_73", "Администратор ID: [x], име: [y]");
