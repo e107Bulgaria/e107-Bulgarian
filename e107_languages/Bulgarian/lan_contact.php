4e0ae4e5d6bccd5ca1801b7a3417f82baeb8dc32
Veselin Totev <v.totev@probg.com>
2026-08-07 15:25:46 +0300

    feat(i18n): migrate Bulgarian contact language file

---
+++ e107_languages/Bulgarian/lan_contact.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Contact language file - Bulgarian
+ *
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ */
+
+define("LAN_CONTACT_00", "Контакти");
+define("LAN_CONTACT_01", "Данни за контакт");
+define("LAN_CONTACT_02", "Форма за контакт");
+define("LAN_CONTACT_03", "Въведете името си");
+define("LAN_CONTACT_04", "Въведете имейл адреса си");
+define("LAN_CONTACT_05", "Тема на съобщението");
+define("LAN_CONTACT_06", "Съобщение");
+define("LAN_CONTACT_07", "Изпрати копие на съобщението на моя имейл адрес");
+define("LAN_CONTACT_08", "Изпращане");
+define("LAN_CONTACT_09", "Съобщението ви беше изпратено успешно.");
+define("LAN_CONTACT_10", "Възникна проблем при изпращането на съобщението.");
+define("LAN_CONTACT_11", "Проверете имейл адреса си и изпратете формата отново.");
+define("LAN_CONTACT_12", "Съобщението е твърде кратко.");
+define("LAN_CONTACT_13", "Моля, въведете тема.");
+
+define("LAN_CONTACT_14", "Изпращане на съобщение до");
+define("LAN_CONTACT_15", "Въведеният код е неправилен");
+define("LAN_CONTACT_16", "За да използвате тази форма, трябва да сте [регистриран] и да сте влезли в профила си.");
+define("LAN_CONTACT_17", "Моля, въведете името си.");
+define("LAN_CONTACT_18", "Моля, въведете имейл адреса си.");
+define("LAN_CONTACT_19", "Моля, въведете тема на съобщението.");
+define("LAN_CONTACT_20", "Моля, въведете съобщението си.");
+define("LAN_CONTACT_21", "Съгласявам се този уебсайт да съхранява изпратената от мен информация, за да може да отговори на запитването ми");
+define("LAN_CONTACT_22", "Политика за поверителност");
+define("LAN_CONTACT_23", "Политиката ни за поверителност е достъпна тук: [x]");
+define("LAN_CONTACT_24", "Съгласие за обработване на лични данни");
