81d539e3c452cbef1c3265b4b65d26c2bbeb0a8f
Veselin Totev <v.totev@probg.com>
2026-08-07 18:26:04 +0300

    feat(i18n): translate bundled theme language files

---
+++ e107_themes/_blank/languages/Bulgarian.php
@@ -0,0 +1,11 @@
+<?php
+define("EMESSLAN_TITLE_INFO", "Системна информация");
+define("EMESSLAN_TITLE_SUCCESS", "Успешно");
+define("EMESSLAN_TITLE_WARNING", "Предупреждение");
+define("EMESSLAN_TITLE_DEBUG", "Системно отстраняване на грешки");
+
+define("LAN_THEME_1", "Коментарите за този елемент са изключени");
+define("LAN_THEME_2", "Преглед/публикуване на коментар: ");
+define("LAN_THEME_3", "Прочетете още...");
+define("LAN_THEME_5", "Публикувано от ");
+define("LAN_THEME_6", "на ");
---
+++ e107_themes/bootstrap3/languages/Bulgarian_admin.php
@@ -0,0 +1,13 @@
+<?php
+
+define("LAN_THEMEPREF_00", "Брандиране:");
+define("LAN_THEMEPREF_01", "Подравняване на навигационната лента:");
+define("LAN_THEMEPREF_02", "Позиция на регистрация/вход:");
+define("LAN_THEMEPREF_03", "Bootswatch стилове:");
+define("LAN_THEMEPREF_04", "Име на сайта");
+define("LAN_THEMEPREF_05", "Лого");
+define("LAN_THEMEPREF_06", "Лого &amp; име на сайта");
+define("LAN_THEMEPREF_07", "вляво");
+define("LAN_THEMEPREF_08", "вдясно");
+define("LAN_THEMEPREF_09", "отгоре");
+define("LAN_THEMEPREF_10", "отдолу");
